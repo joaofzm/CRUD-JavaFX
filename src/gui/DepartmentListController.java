7756175a46032f946c637b28099c6640332d258d
unknown <joao.fzm@hotmail.com>
2021-10-23 19:29:10 -0300

    Created DepartmentService -> DeparmentList fxml view can now show received data

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,13 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,26 +15,35 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service;
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
+
 	@FXML
-	private TableColumn<Department,Integer> tableColumnId;
-	
+	private TableColumn<Department, Integer> tableColumnId;
+
 	@FXML
-	private TableColumn<Department,String> tableColumnName;
-	
+	private TableColumn<Department, String> tableColumnName;
+
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("btNewAction");
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service=service;		
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -39,8 +52,16 @@ public class DepartmentListController implements Initializable {
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
+
 		tableViewDepartment.prefHeightProperty().bind(Main.getMainScene().getWindow().heightProperty());
 	}
 
+	public void updateTableView() {
+		if (service == null) 
+			throw new IllegalStateException("Service is null");
+		
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 }
